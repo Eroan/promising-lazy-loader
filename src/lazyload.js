2063af5a3a5ddde82a1bf9ac301afad8da526df0
Eroan <eroan@mac.com>
2017-08-14 18:24:00 +0200

    Addition of comments

--- src/lazyload.js
+++ src/lazyload.js
@@ -1,56 +1,66 @@
 !(function(a, e) {
- function f() {
-  var d = 0;
-  if (e.body && e.body.offsetWidth) {
-   d = e.body.offsetHeight
-  }
-  if (a.innerWidth && a.innerHeight) {
-   d = a.innerHeight
-  }
-  return d
- }
 
- function b(g) {
-  var d = ot = 0;
-  if (g.offsetParent) {
-   do {
-    d += g.offsetLeft;
-    ot += g.offsetTop
-   } while (g = g.offsetParent)
-  }
-  return {
-   left: d,
-   top: ot
-  }
- }
 
- function doload(image){
-   return new Promise(function(resolve) {
-     image.src = image.getAttribute("data-original");
-     resolve(image);
-   });
- }
-
- function dounload(image){
-   return new Promise(function(resolve) {
-     image.removeAttribute("data-original");
-     resolve(image);
-   });
- }
-
- function c() {
-  var l = e.querySelectorAll("[data-original]");
-  var j = a.pageYOffset || e.documentElement.scrollTop || e.body.scrollTop;
-  var d = f();
-  for (var k = 0; k < l.length; k++) {
-   var h = l[k];
-   var g = b(h).top - 200;
-   if (g < (d + j)) {
-    doload(h)
-    .then(function(response){dounload(response)})
-   }
+  function f() {
+    var d = 0;
+    if (e.body && e.body.offsetWidth) {
+      d = e.body.offsetHeight;
+    }
+    if (a.innerWidth && a.innerHeight) {
+      d = a.innerHeight;
+    }
+    return d;
   }
- }
+
+  function b(g) {
+    var d = ot = 0;
+    if (g.offsetParent) {
+      do {
+        d += g.offsetLeft;
+        ot += g.offsetTop;
+      } while (g = g.offsetParent)
+    }
+    return {
+      left: d,
+      top: ot
+    }
+  }
+
+  // Image is loaded from data-original attribute
+  function doload(image) {
+    return new Promise(function(resolve) {
+      image.src = image.getAttribute("data-original");
+      resolve(image);
+    });
+  }
+
+  // data-original attribute is deleted to avoid multiple loadings
+  function dounload(image) {
+    return new Promise(function(resolve) {
+      image.removeAttribute("data-original");
+      resolve(image);
+    });
+  }
+
+  // Main function that checks if images are on the visible window
+  function c() {
+    var l = e.querySelectorAll("[data-original]");
+    var j = a.pageYOffset || e.documentElement.scrollTop || e.body.scrollTop;
+    var d = f();
+    for (var k = 0; k < l.length; k++) {
+      var h = l[k];
+      var g = b(h).top - 200;
+      if (g < (d + j)) {
+        doload(h)
+          .then(function(response) {
+            dounload(response);
+          })
+      }
+    }
+  }
+
+  // We attach events to loading and scrolling
   a.addEventListener("DOMContentLoaded", c, false);
-  a.addEventListener("scroll", c, false)
+  a.addEventListener("scroll", c, false);
+
 })(window, document);
